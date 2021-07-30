2edb0ca0830454b24426539926a6726fc89b163d
Lars Panzerbjørn <lars@panzerbjrn.eu>
2021-07-30 10:51:36 +0200

    Fixed up Get-AzRAAccessToken code and added help

--- Functions/Get-AzRAAccessToken.ps1
+++ Functions/Get-AzRAAccessToken.ps1
@@ -1,23 +1,71 @@
-Function Get-AzRAAccessToken([string]$TenantID, [string]$ClientID, [string]$ClientSecret){
-  $TokenEndpoint = {https://login.windows.net/{0}/oauth2/token} -f $TenantID 
-  $ARMResource = "https://management.core.windows.net/";
+Function Get-AzRAAccessToken{
+<#
+	.SYNOPSIS
+		Gets the bearer token needed for REST API calls.
 
-  $Body = @{
-          'resource'= $ARMResource
-          'client_id' = $ClientID
-          'grant_type' = 'client_credentials'
-          'client_secret' = $ClientSecret
-  }
+	.DESCRIPTION
+		Gets the bearer token needed for REST API calls.
 
-  $params = @{
-      ContentType = 'application/x-www-form-urlencoded'
-      Headers = @{'accept'='application/json'}
-      Body = $Body
-      Method = 'Post'
-      URI = $TokenEndpoint
-  }
+	.EXAMPLE
+		$TenantId = "c123456f-a1cd-6fv7-bh73-123r5t6y7u8i9"
+		$ClientId = '1a2s3d4d4-dfhg-4567-d5f6-h4f6g7k933ae'
+		$ClientSecret = '36._ERF567.6FB.XFGY75D-35TGasdrvk467'
+		$Resource = "https://management.core.windows.net/"
+		$SubscriptionId = "1ab3456hg-45g6-864c-34ft-jyki8765fdf3"
 
-  $token = Invoke-RestMethod @params
+		Get-AzRAAccessToken -TenantID $TenantID -ClientID $ClientId -ClientSecret $ClientSecret
 
-  Return "Bearer " + ($token.access_token).ToString()
+	.PARAMETER TenantID
+		This is the tenant ID of your Azure subscription.
+
+	.PARAMETER ClientID
+		This is the ClientID of the Service Principal
+
+	.PARAMETER ClientSecret
+		This is the Client secret that was generated when you secured the Service Principal
+
+	.INPUTS
+		Input is from command line or called from a script.
+
+	.OUTPUTS
+		This will output a bearer token that can be used in future API calls.
+
+	.NOTES
+		Author:				Lars Panzerbjørn
+		Creation Date:		2021.07.30
+		Purpose/Change: Initial script development
+#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)][string]$TenantID,
+		[Parameter(Mandatory)][string]$ClientID,
+		[Parameter(Mandatory)][string]$ClientSecret
+	)
+
+	BEGIN{
+		$TokenEndpoint = "https://login.windows.net/$TenantID/oauth2/token"
+		$ARMResource = "https://management.core.windows.net/"
+	}
+	PROCESS{
+		$Body = @{
+			'resource'= $ARMResource
+			'client_id' = $ClientID
+			'grant_type' = 'client_credentials'
+			'client_secret' = $ClientSecret
+		}
+
+		$InvokeRestMethodSplat = @{
+			ContentType = 'application/x-www-form-urlencoded'
+			Headers = @{'accept'='application/json'}
+			Body = $Body
+			Method = 'Post'
+			URI = $TokenEndpoint
+		}
+
+		$Token = Invoke-RestMethod @InvokeRestMethodSplat
+	}
+	END{
+		Return "Bearer " + ($Token.access_token).ToString()
+	}
 }
